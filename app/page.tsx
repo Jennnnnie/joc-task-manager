1aea9fca7137c2d0fdb46c60bb6104d69ec7418d
Jennie <jen.nie.333.chen@gmail.com>
2024-11-19 19:36:28 -0500

    Added Dark Mode and Light Mode icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useDarkMode } from './utils/DarkModeContext';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
 
 export default function Home() {
   const { darkMode, setDarkMode } = useDarkMode();
@@ -10,9 +11,14 @@ export default function Home() {
       <div className={darkMode ? 'dark' : ''}>
         <button
           onClick={() => setDarkMode(!darkMode)}
-          className='px-4 py-2 bg-slate-500 text-white rounded'
+          className='px-4 py-2 bg-slate-500 text-white rounded flex items-center gap-2'
         >
-          Dark Mode: {JSON.stringify(darkMode)}
+          {darkMode ? (
+            <MdLightMode size={24} className='text-yellow-400' />
+          ) : (
+            <MdDarkMode size={24} className='text-gray-800' />
+          )}
+          <span>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
         </button>
       </div>
     </div>
