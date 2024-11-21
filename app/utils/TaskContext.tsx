aeb7c8aac83477d7eda6be28db44d35256336732
Jennie <jen.nie.333.chen@gmail.com>
2024-11-21 09:19:32 -0500

    Added TaskContext.tsx to manage tasks

---
+++ app/utils/TaskContext.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import React, {
+  createContext,
+  ReactNode,
+  useReducer,
+  useEffect,
+  useContext,
+} from 'react';
+
+type Task = {
+  id: string;
+  name: string;
+  description: string;
+  dueDate: string;
+};
+
+type TaskAction =
+  | { type: 'addTask'; payload: Task }
+  | { type: 'editTask'; payload: Task }
+  | { type: 'deleteTask'; payload: string }
+  | { type: 'setTasks'; payload: Task[] };
+
+const TasksReducer = (state: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case 'addTask':
+      return [...state, action.payload];
+    case 'editTask':
+      return state.map((task) =>
+        task.id === action.payload.id ? action.payload : task
+      );
+    case 'deleteTask':
+      return state.filter((task) => task.id !== action.payload);
+    case 'setTasks':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const TasksContext = createContext<any>(null);
+
+export const TasksProvider = ({ children }: { children: ReactNode }) => {
+  const [state, dispatch] = useReducer(TasksReducer, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('tasks') || '[]');
+    if (storedData.length > 0)
+      dispatch({ type: 'setTasks', payload: storedData });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(state));
+  }, [state]);
+
+  return (
+    <TasksContext.Provider value={[state, dispatch]}>
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export const useTasks = () => {
+  const context = useContext(TasksContext);
+  if (!context) throw new Error('useTasks must be used within a Provider!');
+  return context; // Returns state and dispatch
+};
